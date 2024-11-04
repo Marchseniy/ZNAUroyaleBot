f7b784ac8aca3c134109b83f7dba09f5f9322a4f
Marchseniy <145715050+Marchseniy@users.noreply.github.com>
2024-11-04 16:06:10 +0500

    Bug fixes

--- src/main/java/com/marchseniy/ZNAUroyaleBot/ZnauRoyaleBotApplication.java
+++ src/main/java/com/marchseniy/ZNAUroyaleBot/ZnauRoyaleBotApplication.java
@@ -2,19 +2,12 @@ package com.marchseniy.ZNAUroyaleBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.marchseniy.ZNAUroyaleBot.database.repositories")
 public class ZnauRoyaleBotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZnauRoyaleBotApplication.class, args);
-	}
-
-	@GetMapping("/healthz")
-	public String healthCheck() {
-		return "OK";
 	}
 }
